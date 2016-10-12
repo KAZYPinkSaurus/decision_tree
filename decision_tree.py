e3df7b57d852973a65df1ab179324cff3152d7ab
Nishi Kazuya <kwac@Kwacs-MacBook-Pro.local>
2016-10-12 17:55:16 +0900

    関数定義（外側から使えるもの）

---
+++ decision_tree.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Decision_tree:
+    
+    #学習
+    def learn(self,exData,objData):
+        pass
+    
+    #学習結果を用いて識別する
+    def identify(self,data):
+        pass
+    
+    #テストデータを用いて汎化性能を調べる
+    def test(self,exData,objData):
+        pass
